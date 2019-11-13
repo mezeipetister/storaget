5343a7613dbcbb78e3791bcee147ec51614d20ae
Peter Mezei <mezeipetister@gmail.com>
2019-11-13 17:54:44 +0100

    small step ahead

--- src/lib.rs
+++ src/lib.rs
@@ -18,60 +18,157 @@
 mod prelude;
 pub use prelude::*;
 
+// Need this?
+pub trait StorageHasID {
+    fn get_id(&self) -> &str;
+}
+
+// Really?
+pub trait StorageObject<'a> {
+    fn load(&'a mut self, data: &str) -> StorageResult<()>;
+}
+
 pub struct Storage<T> {
     data: T,
     path: &'static str,
 }
 
-pub trait StorageObject {}
-
-pub trait LoadFrom<T> {
-    fn load_from(path: &str) -> StorageResult<T>;
-}
-
-impl<T> LoadFrom<T> for T
+impl<'a, T: 'a> Storage<Vec<T>>
 where
-    T: StorageObject,
+    T: StorageObject<'a> + StorageHasID,
 {
-    fn load_from(path: &str) -> StorageResult<T> {
-        Err(Error::InternalError("oo".into()))
-    }
-}
-
-impl<T, U> LoadFrom<U> for Vec<T>
-where
-    T: StorageObject,
-{
-    fn load_from(path: &str) -> StorageResult<U> {
-        Err(Error::InternalError("oo".into()))
-    }
-}
-
-impl<T> Storage<T>
-where
-    T: LoadFrom<T>,
-{
-    pub fn load_from(path: &'static str) -> StorageResult<Storage<T>> {
-        Ok(Storage {
-            data: T::load_from(path)?,
+    pub fn new(path: &'static str) -> Self {
+        Storage {
+            data: Vec::new(),
             path,
-        })
+        }
+    }
+    pub fn get_by_id(&'a mut self, id: &str) -> Option<DataObject<T>> {
+        for item in &mut self.data {
+            if item.get_id() == id {
+                return Some(DataObject {
+                    data: item,
+                    path: self.path,
+                });
+            }
+        }
+        None
+    }
+    pub fn add_to_storage(&'a mut self, new_object: T) -> StorageResult<()> {
+        self.data.push(new_object);
+        Ok(())
     }
 }
 
-pub fn demo_func() -> u32 {
-    7
+#[derive(Debug)]
+pub struct DataObject<'a, T> {
+    data: &'a mut T,
+    path: &'static str,
+}
+
+impl<'a, T> DataObject<'a, T> {
+    pub fn get(&'a self) -> &'a T {
+        self.data
+    }
+    pub fn get_mut(&'a mut self) -> &'a mut T {
+        self.data
+    }
+    pub fn update<F: 'a, R>(&'a mut self, mut f: F) -> R
+    where
+        F: FnMut(&mut T) -> R,
+    {
+        f(self.data)
+    }
 }
 
 #[test]
-fn simple_test() {
-    // struct User {
-    //     id: String,
-    //     name: String,
-    // }
-    // impl StorageObject for u32 {}
-    // impl StorageObject for User {}
-    // let users: Storage<Vec<User>> = Storage::load_from("data").unwrap();
-    // let user: Storage<User> = Storage::load_from("data").unwrap();
-    assert_eq!(demo_func(), 7);
+fn basic_test() {
+    struct User {
+        id: String,
+        name: String,
+    }
+    impl User {
+        fn new(id: &str, name: &str) -> Self {
+            User {
+                id: id.into(),
+                name: name.into(),
+            }
+        }
+        fn set_name(&mut self, name: &str) {
+            self.name = name.into();
+        }
+    }
+    impl<'a> StorageObject<'a> for User {
+        fn load(&'a mut self, data: &str) -> StorageResult<()> {
+            Ok(())
+        }
+    }
+    impl StorageHasID for User {
+        fn get_id(&self) -> &str {
+            &self.id
+        }
+    }
+    let mut storage: Storage<Vec<User>> = Storage::new("data");
+    storage.add_to_storage(User::new("1", "Kriszti")).unwrap();
+    storage.add_to_storage(User::new("2", "Peti")).unwrap();
+    storage.add_to_storage(User::new("3", "Gabi")).unwrap();
+
+    // let mut a = vec![1,2,3,4,5];
+    // a.iter_mut();
+
+    assert_eq!(storage.get_by_id("1").unwrap().get().name, "Kriszti");
+    assert_eq!(storage.get_by_id("2").unwrap().get().name, "Peti");
+    assert_eq!(storage.get_by_id("3").unwrap().get().name, "Gabi");
+    storage
+        .get_by_id("3")
+        .unwrap()
+        .update(|u| u.set_name("Gabi!"));
+    let res = storage.get_by_id("3").unwrap().update(|u| -> bool {
+        u.set_name("Gabi!!!!");
+        true
+    });
+    assert_eq!(res, true);
+    assert_eq!(storage.get_by_id("3").unwrap().get().name, "Gabi!!!!");
+    assert_eq!(storage.get_by_id("4").is_none(), true);
 }
+
+// pub struct Storage<T> {
+//     data: T,
+//     path: &'static str,
+// }
+
+// pub trait StorageObject {}
+
+// pub trait LoadFrom<T> {
+//     fn load_from(path: &str) -> StorageResult<T>;
+// }
+
+// impl<T> LoadFrom<T> for T
+// where
+//     T: StorageObject,
+// {
+//     fn load_from(path: &str) -> StorageResult<T> {
+//         Err(Error::InternalError("oo".into()))
+//     }
+// }
+
+// impl<T, U> LoadFrom<U> for Vec<T>
+// where
+//     T: StorageObject,
+// {
+//     fn load_from(path: &str) -> StorageResult<U> {
+//         Err(Error::InternalError("oo".into()))
+//     }
+// }
+
+// impl<T> Storage<T>
+// where
+//     T: LoadFrom<T>,
+// {
+//     pub fn load_from(path: &'static str) -> StorageResult<Storage<T>> {
+//         Ok(Storage {
+//             data: T::load_from(path)?,
+//             path,
+//         })
+//     }
+// }
